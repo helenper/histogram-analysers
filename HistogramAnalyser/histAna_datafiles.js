2a636e1250b3389db3a84c9319082c0de52482bc
Helen Persson <helen.persson@cern.ch>
2020-09-18 17:47:15 +0200

    Started work on webpage to include already exiting tool from ATLAS and to start work on possibility (JavaScript) for uploading files/reading already existing datafiles

---
+++ HistogramAnalyser/histAna_datafiles.js
@@ -0,0 +1,5 @@
+var newDataButton = document.getElementById("newDataFile")
+var oldDataButton = document.getElementById("oldDataFile")
+var date = document.getElementById("date")
+var institute = document.getElementById("institute")
+
