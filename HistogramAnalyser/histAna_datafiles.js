3db01f8b72ff0ce29d59d1af7baade6b950a2644
helenper <helen.persson91@gmail.com>
2020-09-20 11:33:04 +0200

    Some changes

--- HistogramAnalyser/histAna_datafiles.js
+++ HistogramAnalyser/histAna_datafiles.js
@@ -3,3 +3,21 @@ var oldDataButton = document.getElementById("oldDataFile")
 var date = document.getElementById("date")
 var institute = document.getElementById("institute")
 
+function uploadNewFile(){
+  this.innerHTML = "You added a new data file";
+
+}
+
+function selectOldFile(){
+  d = date.options[date.selectedIndex].value;
+  inst = institute.options[institute.selectedIndex].value;
+
+  console.log(d,inst)
+
+
+
+}
+
+newDataButton.addEventListener("click", uploadNewFile )
+
+oldDataButton.addEventListener("click", selectOldFile )
