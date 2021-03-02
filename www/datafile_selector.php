7db4f9e93ecaec0b5cb4db7e7140bdc26557c47f
helenper <helen.persson91@gmail.com>
2021-03-02 13:17:49 +0100

    Working on datafile selector

--- www/datafile_selector.php
+++ www/datafile_selector.php
@@ -1,21 +1,38 @@
 <?php
 //session_start();
+
 include_once("header.php");
 
   // Get data from form - upload new data
+  echo '<pre>';
+  var_dump($_SESSION);
+  echo '</pre>';
 
   $_SESSION['uploadDate'] = $_POST["uploadDate"];
-  $_SESSION['group'] = $_POST["group"];
-  $_SESSION['file'] = $_POST["inputFile"];
   $_SESSION['institute'] = $_POST["institute"];
+  $_SESSION['group'] = $_POST["group"];
+  $_SESSION['file'] = $_FILES["inputFile"]; //$_POST["inputFile"];
 
-  //echo $_SESSION['userName'];
+  echo '<pre>';
+  var_dump($_SESSION);
+  echo '</pre>';
+  //$file = $_FILES["inputFile"];
+  //$institute = $_POST["institute"];
+  //$uploadDate = $_POST["uploadDate"];
+  //$group = $_POST["group"];
 
-  $file = $_FILES["inputFile"];
-  $institute = $_POST["institute"];
-  $uploadDate = $_POST["uploadDate"];
-  $group = $_POST["group"];
-
+  if ( isset($_SESSION['uploadDate']) && !isset($_SESSION['file'])) {
+    echo "All is NOT good with file.";
+    //header("student_page.php?error=upload");
+  }
+  elseif (!isset($_SESSION['uploadDate']) || !isset($_SESSION['group']) || !isset($_SESSION['institute']) ){
+    echo "All is NOT good with some form data.";
+      //header("student_page.php?error=info");
+  }
+  else {
+    echo "All is good.";
+    echo $_SESSION['uploadDate'], $_SESSION['institute'], $_SESSION['group'], $_SESSION['file'];
+  }
 
 
   /*if (!isset($_SESSION['institute'])) {
@@ -27,22 +44,33 @@ include_once("header.php");
 */
   // Get data from form - old data -
   // "NOT IMPLEMENTED"
+  /*
   $date = $_POST["date"];
   $institute = $_POST["institute"];
 
   // Handle uploaded file
 
+  $dataLocUploadFolder = "data/" . $uploadDate . "/" . $institute;
+
   $dataLocUpload = "data/" . $uploadDate . "/" . $institute . "/" . $group . ".csv";
 
-  move_uploaded_file($file["tmp_name"], $dataLocUpload);
-  $_SESSION['dataLocUpload'] = $dataLocUpload;
+    $_SESSION['dataLocUpload'] = $dataLocUpload;
+
+  //if (!file_exists($dataLocUploadFolder)) {
+  //  mkdir($dataLocUploadFolder, 0777, true);
+//}
+
+  //move_uploaded_file($file["tmp_name"], /data/ );//$dataLocUpload);
+
+  echo "Test";
 
 
+*/
 
   //echo $dataLocUpload;
   // Relocate to student page
 
   //header("Location: student_page.php");
-  header("Location: /HistogramAnalyser/crossfilter_masterclass_web.php") //. "?file=" . $dataLocUpload)
+  //header("Location: ../HistogramAnalyser/crossfilter_masterclass_web.php") //. "?file=" . $dataLocUpload)
   //echo $dataLocUpload;
 ?>
---
+++ www/datafile_selector_new.php
@@ -0,0 +1,29 @@
+<?php
+
+//include_once("header.php");
+
+echo "TEST";
+
+
+
+
+/*
+
+echo '<pre>';
+var_dump($_SESSION);
+echo '</pre>';
+
+$_SESSION['uploadDate'] = $_POST['date_upload_new'];
+$_SESSION['institute_new'] = $_POST['institute_new'];
+$_SESSION['group_new'] = $_POST['group_new'];
+$_SESSION['inputFile'] = $_FILES['inputFile'];
+
+echo '<pre>';
+var_dump($_SESSION);
+echo '</pre>';
+
+*/
+
+
+
+?>
